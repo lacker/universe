1986025d014e5453d34cefa24f32faecd5e939d7
Kevin Lacker <lacker@gmail.com>
2015-09-06 23:00:15 -0700

    spin a green cube

--- js/universe.js
+++ js/universe.js
@@ -11,17 +11,19 @@ function main() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  // Make a cube
+  // Make a single green cube
   var geometry = new THREE.BoxGeometry(1, 1, 1);
-  var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+  var material = new THREE.MeshBasicMaterial({color: 0x66cc33});
   var cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
+
   camera.position.z = 5;
   
   // Start rendering
-  // TODO: get rid of weird subfunction
   function render() {
     requestAnimationFrame(render);
+    cube.rotation.x += 0.01 * Math.random();
+    cube.rotation.y += 0.01 * Math.random();
     renderer.render(scene, camera);
   }
   render();
