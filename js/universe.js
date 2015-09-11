95f9418c38d90d3769f28196ea3087a38ca1336a
Kevin Lacker <lacker@gmail.com>
2015-09-10 22:46:02 -0700

    poke consts

--- js/universe.js
+++ js/universe.js
@@ -8,7 +8,7 @@ function main() {
   // Set up rendering
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera(
-    75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    75, window.innerWidth / window.innerHeight, 1, 10000);
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
