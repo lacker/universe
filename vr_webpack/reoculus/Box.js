e11e67ea5faf4b58717a9291314133d76ffc9694
Kevin Lacker <lacker@gmail.com>
2015-09-18 09:43:27 -0700

    box takes color prop

--- vr_webpack/reoculus/Box.js
+++ vr_webpack/reoculus/Box.js
@@ -3,11 +3,21 @@ import Three from 'three';
 
 export default class Box extends React.Component {
   render() {
+    console.log("rendering Box with props:");
+    console.log(this.props);
+    console.log("---");
     var width = this.props.width || 1;
     var height = this.props.height || 1;
     var depth = this.props.depth || 1;
     var geometry = new Three.BoxGeometry(width, height, depth);
-    var material = this.props.material || new Three.MeshNormalMaterial();
+    var material = this.props.material;
+    if (!material) {
+      if (this.props.color) {
+        material = new Three.MeshBasicMaterial({color: this.props.color});
+      } else {
+        material = new Three.MeshNormalMaterial();
+      }
+    }
     return (
       <mesh
         {...this.props}
